ea020abd480eef12cf3cf685a5744c21de2557c3
CodeWithOtee <adekoya.omotoyosi@outlook.com>
2025-09-07 18:16:57 +0100

    Made homepage logo responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-white">
-      <div className="flex items-center justify-center">
+      <div className="w-11/12 max-w-[600px] sm:max-w-[800px] md:max-w-[1000px]">
         <Image
-          src="/nobillspnglogo.png" // make sure this matches your file name
+          src="/nobillspnglogo.png"
           alt="NOBILLS Logo"
-          width={800}  // bigger width
-          height={800} // bigger height
+          width={1000} // max width
+          height={1000} // max height
+          className="w-full h-auto"
           priority
         />
       </div>
